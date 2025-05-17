b67b44147f72c5d1a3ced364cec0988788dd51b6
xturo05 <khoakim09@gmail.com>
2025-05-17 22:04:40 +0700

    fix turbo

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
-    });
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.mjs$/,
+  //     include: /node_modules/,
+  //     type: "javascript/auto",
+  //   });
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
